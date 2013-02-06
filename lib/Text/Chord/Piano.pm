16c7d6b85fd9bf3260a6d4ed5427e548d8b17b25
bayashi <bayashi@cpan.org>
2013-02-06 23:16:11 +0900

    Bumped version

--- lib/Text/Chord/Piano.pm
+++ lib/Text/Chord/Piano.pm
@@ -9,7 +9,7 @@ use Music::Chord::Note;
 use base qw(Class::Accessor::Fast);
 __PACKAGE__->mk_accessors( qw(finger) );
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 my $cn = Music::Chord::Note->new();
 
